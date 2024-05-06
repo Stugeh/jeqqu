c9101d7eab928b8de0938d73e1c073ca480b2bf6
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-05-06 15:14:56 +0000

    Switched tui library to the same one used by lazygit

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
 )
 
 func main() {
-	box := tview.
-		NewBox().
-		SetBorder(true).
-		SetTitle("JEQQU")
-
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
 	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
