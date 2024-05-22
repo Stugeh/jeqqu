296ff7b4bade7cd326c910597a975db88fe8c5b1
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-05-22 19:54:58 +0000

    added comments for functions

--- src/utils/jqApi.go
+++ src/utils/jqApi.go
@@ -2,6 +2,7 @@ package utils
 
 import "os/exec"
 
+// Executes a jq query
 func ExecuteQueryCommand(path, query string) ([]byte, error) {
 	out, err := exec.Command("jq", query, path).Output()
 
@@ -13,6 +14,7 @@ func ExecuteQueryCommand(path, query string) ([]byte, error) {
 	return out, err
 }
 
+// Prints latest temp file
 func PrettyPrintLatest(bytes []byte) {
 	// _, err := exec.Command(jq", "'.'" ).Output()
 	//
