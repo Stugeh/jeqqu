7689377135142b4c73039fc39b40860e33608dc3
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-05-22 20:04:20 +0000

    removed redundant function

--- src/utils/files.go
+++ src/utils/files.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-	"path/filepath"
 	"sort"
 	"time"
 )
@@ -16,33 +15,13 @@ func createTempFilePath() string {
 	return TEMP_DIR + fmt.Sprint(time.Now().Unix()) + "_temp.json"
 }
 
-func GetNewestTempFilePath() (string, error) {
-	dir, err := os.Open(TEMP_DIR)
-
-	if err != nil {
-		return "", err
-	}
-
-	defer dir.Close()
-
-	fileInfos, err := dir.Readdir(-1)
-	if err != nil {
-		return "", err
-	}
-
+func SortFilesByModTimeAsc(fileInfos []fs.FileInfo) {
 	sort.Slice(fileInfos, func(i, j int) bool {
-		return fileInfos[i].ModTime().After(fileInfos[j].ModTime())
+		return fileInfos[i].ModTime().Before(fileInfos[j].ModTime())
 	})
-
-	if len(fileInfos) > 0 {
-		newestFilePath := filepath.Join(TEMP_DIR, fileInfos[0].Name())
-		return newestFilePath, nil
-	}
-
-	return "", fmt.Errorf("no files found in directory")
 }
 
-func SortFileInfoAsc(fileInfos []fs.FileInfo) {
+func SortFilesByModTimeDesc(fileInfos []fs.FileInfo) {
 	sort.Slice(fileInfos, func(i, j int) bool {
 		return fileInfos[i].ModTime().Before(fileInfos[j].ModTime())
 	})
@@ -57,7 +36,7 @@ func NewTempFile(bytes []byte) (string, error) {
 
 	// Delete oldest temp file if Max reached
 	if len(fileInfos) > MAX_FILES {
-		SortFileInfoAsc(fileInfos)
+		SortFilesByModTimeAsc(fileInfos)
 		os.Remove(TEMP_DIR + fileInfos[0].Name())
 	}
 
